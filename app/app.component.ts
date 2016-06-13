9b111199e0a690945a85869020f26726c6a51756
Tuddenham <dtuddenham@lavastorm.com>
2016-06-13 13:10:25 +0100

    Re-factored to use multiple components, next is services: https://angular.io/docs/ts/latest/tutorial/toh-pt4.html

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,6 @@
 import {Component} from '@angular/core';
-
-export class Hero {
-	id: number;
-	name: string;
-}
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @Component({
     selector: 'my-app',
@@ -15,16 +12,7 @@ export class Hero {
 			<span class="badge">{{hero.id}}</span> {{hero.name}}
 		  </li>
 		</ul>
-		<div *ngIf="selectedHero">
-			<h2>{{selectedHero.name}} details!</h2>
-			<div>
-				<label>id: </label>{{selectedHero.id}}
-			</div>
-			<div>
-				<label>name: </label>
-				<input [(ngModel)]="selectedHero.name" placeholder="name">
-			</div>
-		</div>
+		<my-hero-detail [hero]="selectedHero"></my-hero-detail>
 	`,
 	styles:[`
 	  .selected {
@@ -74,13 +62,14 @@ export class Hero {
 		margin-right: .8em;
 		border-radius: 4px 0 0 4px;
 	  }
-	`]
+	`],
+	directives: [HeroDetailComponent]
 
 })
 export class AppComponent { 
 	title = 'Tour of Heroes';
 	selectedHero: Hero;
-	public heroes = HEROES;
+	heroes = HEROES;
 	onSelect(hero: Hero) {
 		this.selectedHero = hero;
 	}
